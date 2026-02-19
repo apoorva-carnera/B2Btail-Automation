fb6a4269193bc4e8e43f3d216723a7528b3efa1d
apoorvamarathe <apoorva@getcarnera.com>
2026-02-19 10:59:31 +0530

    Content-planner test cases in progress

--- pages/content-planner.js
+++ pages/content-planner.js
@@ -4,13 +4,52 @@ export default class contentPlannerPage {
   constructor(page) {
     this.page = page;
 
-    this.left_nav_option = page.locator("//span[text()='Content Planner']");
-    this.page_title = page.locator("//h1[text()='Content Planner']");
+    this.Left_Nav_Option = page.locator("//span[text()='Content Planner']");
+    this.Page_Title = page.locator("//h1[text()='Content Planner']");
+    this.Month = page.getByRole("button", { name: "Month" });
+    this.Week = page.getByRole("button", { name: "Week" });
+    this.Day = page.getByRole("button", { name: "Day" });
+    this.List = page.getByRole("button", { name: "List" });
+    this.Current_Month = page.locator(".font-semibold.text-foreground");
+    this.Current_Day = page.locator(".calendar-cell-today");
+    this.Content_Planner = page.locator(".bg-brand-accent.text-sidebar-text");
+    this.Post_Title = page.locator("#title");
+    this.Post_Content = page.locator("#content");
+    this.Platforms = page.getByRole("checkbox");
   }
 
   async verifyUIElements() {
-    await this.left_nav_option.click();
-    await expect(this.left_nav_option).toBeVisible();
-    await expect(this.page_title).toBeVisible();
+    await this.Left_Nav_Option.click();
+    await expect(this.Page_Title).toBeVisible();
+    await expect(this.Left_Nav_Option).toHaveText("Content Planner");
+    await expect(this.Month).toBeVisible();
+    //await expect(this.Week).toBeVisible();
+    //await expect(this.Day).toBeVisible();
+    //await expect(this.List).toBeVisible();
+  }
+
+  async createNewPostForCurrentDay() {
+    let current_month = await this.Current_Month.textContent();
+    console.log("Current month is: " + current_month);
+    let current_day = await this.Current_Day.textContent();
+    console.log("Today's date is: " + current_day);
+    await this.Current_Day.click();
+    await this.addNewPost();
+  }
+
+  async addNewPost() {
+    await this.Post_Title.fill("Test");
+    await this.Post_Content.fill("Test");
+    let platform_count = await this.Platforms.count();
+    console.log("Platform count is: " + platform_count);
+
+    for (let i = 0; i < platform_count; i++) {
+      await this.Platforms.nth(i).click();
+      let platform_name = await this.page
+        .locator("//div[@class='grid grid-cols-2 gap-3']/div/label")
+        .nth(i)
+        .textContent();
+      console.log("Platform selected is: " + platform_name);
+    }
   }
 }
--- tests/content-planner.spec.js
+++ tests/content-planner.spec.js
@@ -2,14 +2,21 @@ import { expect, test } from "@playwright/test";
 import basePage from "../pages/base-page";
 import contentPlannerPage from "../pages/content-planner";
 
-test.describe("B2B Automation test cases", { tag: ["@sanity"] }, async () => {
-  test("Test case1: Verify UI elements on Content Planner page", async ({
-    page,
-  }) => {
-    const basepage = new basePage(page);
-    const cont_planner_page = new contentPlannerPage(page);
-    await basepage.goTo();
-    await basepage.login();
-    await cont_planner_page.verifyUIElements();
-  });
-});
+test.describe(
+  "B2B Automation test cases",
+  { tag: ["@Regression"] },
+  async () => {
+    test("Test case1: Verify UI elements on Content Planner page", async ({
+      page,
+    }) => {
+      const basepage = new basePage(page);
+      const cont_planner_page = new contentPlannerPage(page);
+      await basepage.goTo();
+      await basepage.login();
+      await cont_planner_page.verifyUIElements();
+      await cont_planner_page.createNewPostForCurrentDay();
+    });
+
+    test("Test Case2: Create Post", async ({ page }) => {});
+  },
+);
